93b38d934a14729dd52fe6fbd88b91c756f5d4b6
user <kstdasha2013@gmail.com>
2025-01-05 20:28:00 +0300

    13

--- src/Engine.php
+++ src/Engine.php
@@ -2,60 +2,37 @@
 
 namespace BrainGames\Engine;
 
-function welcome()
+use function cli\line;
+use function cli\prompt;
+
+function greeting(): string
 {
-    print_r("Welcome to the Brain Games!\n");
+    line('Welcome to the Brain Games!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    
+    return $name;
 }
 
-function userCanQuestAns(string $userName, string $trueAns)
+function task(string $text)
 {
-    $userAns = readline("Your answer: ");
-    if ($userAns === $trueAns) {
-        print_r("Correct!\n");
-        return true;
-    } else {
-        print_r("'{$userAns}' is wrong answer ;(. Correct answer was '{$trueAns}'.\n");
-        print_r("Let's try again, {$userName}!\n");
-        return false;
+    line($text);
+}
+
+function gameProcess(string $gameName, string $name) 
+{
+    $result = '';
+    for ($rightAnswerCount = 1; $rightAnswerCount < 4; $rightAnswerCount++) {
+        if (is_callable('BrainGames\Games\\' . $gameName)) {
+            $result = call_user_func('BrainGames\Games\\' . $gameName);
+        }
+        if ($result === "Mistake") {
+            line('Let\'s try again, %s!', $name);
+            break;
+        }
+        line('Correct!');
+        if ($rightAnswerCount === 3) {
+            line('Congratulations, %s!', $name);
+        }
     }
-}
-
-function congratulations(string $userName)
-{
-    echo "Congratulations, {$userName}!\n";
-}
-
-function roundQuest(string $str)
-{
-    echo "Question: {$str}\n";
-}
-
-function printQuestList(array $list)
-{
-    echo "Question:";
-    foreach ($list as $i) {
-        echo " {$i}";
-    }
-    echo "\n";
-}
-
-function calcQuest()
-{
-    echo "What is the result of the expression?\n";
-}
-
-function evenQuest()
-{
-    echo 'Answer "yes" if the number is even, otherwise answer "no".';
-    echo "\n";
-}
-
-function gcdQuest()
-{
-    echo "Find the greatest common divisor of given numbers.\n";
-}
-
-function progressionQuest()
-{
-    echo "What number is missing in the progression?\n";
 }
