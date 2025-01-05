26282525d4300fcc7bca28831967ef374bffb046
user <kstdasha2013@gmail.com>
2025-01-05 20:16:12 +0300

    11

--- src/Engine.php
+++ src/Engine.php
@@ -1,34 +1,61 @@
 <?php
 
-namespace BrainGames;
+namespace Project1\Engine;
 
-use function cli\line;
-use function cli\prompt;
-
-const ITERATIONS_COUNT = 3;
-
-function runEngine($getRightAnswerForRound, $question)
+function welcome()
 {
-    line('Welcome to the Brain Game!');
-    line($question);
-    $name = prompt('May I have your name?');
-    line('Hello, %s!', $name);
+    print_r("Welcome to the Brain Games!\n");
+}
 
-    for ($i = 0; $i < ITERATIONS_COUNT; $i++) {
-        ['roundQuestion' => $roundQuestion, 'rightAnswer' => $rightAnswer] = $getRightAnswerForRound();
-        
-        line("Question: {$roundQuestion}");
-        $answerUser = prompt('Your answer');
-        
-        if ($answerUser == $rightAnswer) {
-            line('Correct!');
-        } else {
-            line("{$answerUser}  is wrong answer ;(. Correct answer was {$rightAnswer}");
-            line("Let's try again, {$name}!");
-            return;
-        }
+function userCanQuestAns(string $userName, string $trueAns)
+{
+    $userAns = readline("Your answer: ");
+    if ($userAns === $trueAns) {
+        print_r("Correct!\n");
+        return true;
+    } else {
+        print_r("'{$userAns}' is wrong answer ;(. Correct answer was '{$trueAns}'.\n");
+        print_r("Let's try again, {$userName}!\n");
+        return false;
     }
-    
-    line("Congratulations, {$name}!");
-    return;
+}
+
+function congratulations(string $userName)
+{
+    echo "Congratulations, {$userName}!\n";
+}
+
+function roundQuest(string $str)
+{
+    echo "Question: {$str}\n";
+}
+
+function printQuestList(array $list)
+{
+    echo "Question:";
+    foreach ($list as $i) {
+        echo " {$i}";
+    }
+    echo "\n";
+}
+
+function calcQuest()
+{
+    echo "What is the result of the expression?\n";
+}
+
+function evenQuest()
+{
+    echo 'Answer "yes" if the number is even, otherwise answer "no".';
+    echo "\n";
+}
+
+function gcdQuest()
+{
+    echo "Find the greatest common divisor of given numbers.\n";
+}
+
+function progressionQuest()
+{
+    echo "What number is missing in the progression?\n";
 }
